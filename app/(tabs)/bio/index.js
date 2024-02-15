d0c5310a99359e2d40502c249ed4eefcf5c7497e
RahulKathayat <rkathayat1103@gmail.com>
2024-02-15 18:13:00 +0530

    commit

--- app/(tabs)/bio/index.js
+++ app/(tabs)/bio/index.js
@@ -1,11 +1,56 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,ScrollView, Pressable ,Image} from 'react-native'
 import React from 'react'
 
 const index = () => {
   return (
-    <View>
-      <Text>index</Text>
-    </View>
+    <ScrollView>
+      <View>
+      <Image
+          style={{ width: "100%", height: 200, resizeMode: "cover" }}
+          source={{
+            uri: "https://static.vecteezy.com/system/resources/thumbnails/018/977/074/original/animated-backgrounds-with-liquid-motion-graphic-background-cool-moving-animation-for-your-background-free-video.jpg",
+          }}
+        />
+        <View>
+          <View>
+            <Pressable
+              style={{
+                padding: 10,
+                backgroundColor: "#DDA0DD",
+                width: 300,
+                marginLeft: "auto",
+                marginRight: "auto",
+                justifyContent: "center",
+                alignItems: "center",
+                borderRadius: 10,
+                // position: "absolute",
+                top: -60,
+                // left: "50%",
+                // transform: [{ translateY: -150 }],
+              }}
+            >
+              <Image
+                style={{
+                  width: 60,
+                  height: 60,
+                  borderRadius: 30,
+                  resizeMode: "cover",
+                }}
+                source={{
+                    uri: "https://images.pexels.com/photos/1042140/pexels-photo-1042140.jpeg?auto=compress&cs=tinysrgb&w=800",
+                }}
+              />
+              <Text style={{ fontSize: 16, fontWeight: "600", marginTop: 6 }}>
+                Bangalore
+              </Text>
+              <Text style={{ marginTop: 4, fontSize: 15 }}>
+                22 years 110 days
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
   )
 }
 
