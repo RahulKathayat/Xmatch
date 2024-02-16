54771f178fda6ebcb6562688bf2aff0d4a3d1c24
RahulKathayat <rkathayat1103@gmail.com>
2024-02-16 16:39:31 +0530

    bio page updated

--- app/(tabs)/bio/index.js
+++ app/(tabs)/bio/index.js
@@ -2,20 +2,23 @@ import {
   StyleSheet,
   Text,
   View,
-  ScrollView,
   Pressable,
   Image,
   TextInput,
+  Button,
+  TouchableOpacity,
+  FlatList,
 } from "react-native";
 import React, { useState } from "react";
+import { ScrollView } from 'react-native-virtualized-view'
 import { Entypo } from "@expo/vector-icons";
 import Carousel from "react-native-snap-carousel";
 
 const index = () => {
   const [options, setOptions] = useState("AD");
   const [description, setDescription] = useState("");
-  const [activeSlide,setActiveSlide] = React.useState(0);
-  //test data 
+  const [activeSlide, setActiveSlide] = React.useState(0);
+  //test data
   const profileImages = [
     {
       image:
@@ -28,6 +31,58 @@ const index = () => {
     {
       image:
         "https://images.pexels.com/photos/7580971/pexels-photo-7580971.jpeg?auto=compress&cs=tinysrgb&w=800",
+    },
+  ];
+  const turnons = [
+    {
+      id: "0",
+      name: "Music",
+      description: "Pop Rock-Indie pick our sound track",
+    },
+    {
+      id: "10",
+      name: "Kissing",
+      description:
+        " It's a feeling of closeness, where every touch of lips creates a symphony of emotions.",
+    },
+    {
+      id: "1",
+      name: "Fantasies",
+      description:
+        "Fantasies can be deeply personal, encompassing diverse elements such as romance",
+    },
+    {
+      id: "2",
+      name: "Nibbling",
+      description:
+        "playful form of biting or taking small, gentle bites, typically done with the teeth",
+    },
+    {
+      id: "3",
+      name: "Desire",
+      description: "powerful emotion or attainment of a particular person.",
+    },
+  ];
+  const data = [
+    {
+      id: "0",
+      name: "Casual",
+      description: "Let's keep it easy and see where it goes",
+    },
+    {
+      id: "1",
+      name: "Long Term",
+      description: "How about a one life stand",
+    },
+    {
+      id: "2",
+      name: "Virtual",
+      description: "Let's have some virtual fun",
+    },
+    {
+      id: "3",
+      name: "Open for Anything",
+      description: "Let's Vibe and see where it goes",
     },
   ];
   const renderImageCarousel = ({ item }) => (
@@ -44,9 +99,7 @@ const index = () => {
         }}
         source={{ uri: item?.image }}
       />
-      <Text
-        style={{ color: "black" , fontSize:17,fontWeight:"500"}}
-      >
+      <Text style={{ color: "black", fontSize: 17, fontWeight: "500" }}>
         {activeSlide + 1}/{profileImages.length}
       </Text>
     </View>
@@ -204,9 +257,8 @@ const index = () => {
       </View>
 
       <View style={{ marginHorizontal: 14, top: -30 }}>
-        {
-          options == "Photos" && (
-          <View style={{top:30}}>
+        {options == "Photos" && (
+          <View style={{ top: 30 }}>
             <Carousel
               data={profileImages}
               renderItem={renderImageCarousel}
@@ -214,8 +266,132 @@ const index = () => {
               itemWidth={300}
               onSnapToItem={(index) => setActiveSlide(index)}
             />
-            <Text style={{left:"40%"}}>Swipe left</Text>
+            <Text style={{ left: "40%" }}>Swipe left</Text>
+            <View style={{ marginTop: 25, gap: 10 }}>
+              <Text>Add a picture of yourself</Text>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  gap: 5,
+                  paddingVertical: 5,
+                  borderRadius: 5,
+                  marginTop: 10,
+                  backgroundColor: "#DCDCDC",
+                }}
+              >
+                <Entypo
+                  style={{ marginLeft: 8 }}
+                  name="image"
+                  size={24}
+                  color="gray"
+                />
+                <TextInput
+                  style={{ color: "gray", marginVertical: 10, width: 200 }}
+                  placeholder="enter your image url"
+                />
+              </View>
+              <Button style={{}} title="Add Image" />
+              <View></View>
+            </View>
           </View>
+        )}
+      </View>
+
+      <View style={{ marginHorizontal: 14, top: -30 }}>
+        {options == "Turn-ons" && (
+        <View>
+          {turnons?.map((item, index) => (
+              <Pressable
+                style={{
+                  backgroundColor: "#FFFDD0",
+                  padding: 10,
+                  marginVertical: 10,
+                }}
+                key={index}
+              >
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Text
+                    style={{
+                      textAlign: "center",
+                      fontSize: 15,
+                      fontWeight: "bold",
+                      flex: 1,
+                    }}
+                  >
+                    {item?.name}
+                  </Text>
+                </View>
+                <Text
+                  style={{
+                    marginTop: 4,
+                    fontSize: 15,
+                    color: "gray",
+                    textAlign: "center",
+                  }}
+                >
+                  {item?.description}
+                </Text>
+              </Pressable>
+            ))}
+        </View>
+        )}
+      </View>
+
+      <View style={{ marginHorizontal: 14, top: -30  }}>
+        {options == "Looking for" && (
+          <>
+            <View>
+              <FlatList
+                columnWrapperStyle={{ justifyContent: "space-between" }}
+                numColumns={2}
+                data={data}
+                renderItem={({ item }) => (
+                  <Pressable
+                    style={{
+                      backgroundColor: "white",
+                      padding: 16,
+                      justifyContent: "center",
+                      alignItems: "center",
+                      width: 150,
+                      margin: 10,
+                      borderRadius: 5,
+                      borderColor: "#fd5c63",
+                      borderWidth: 0.7,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        textAlign: "center",
+                        fontWeight: "500",
+                        fontSize: 13,
+                        color: "black",
+                      }}
+                    >
+                      {item?.name}
+                    </Text>
+                    <Text
+                      style={{
+                        color: "gray",
+                        textAlign: "center",
+                        width: 140,
+                        marginTop: 10,
+                        fontSize: 13,
+                      }}
+                    >
+                      {item?.description}
+                    </Text>
+                  </Pressable>
+                )}
+              />
+            </View>
+          </>
         )}
       </View>
     </ScrollView>
